693c6513866b68abae7f247b2118acf6b6cc66bd
ravenspeak <89217966+ravenspeak@users.noreply.github.com>
2021-11-08 16:34:40 +0000

    Fixed the attribute

--- api/index.js
+++ api/index.js
@@ -14,11 +14,11 @@ const app = express()
 // Static public files
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('Get ready for OpenSea!');
 })
 
-app.get('/api/token/:token_id', function(req, res) {
+app.get('/api/token/:token_id', function (req, res) {
   const tokenId = parseInt(req.params.token_id).toString()
   const person = db[tokenId]
   const bdayParts = person.birthday.split(' ')
@@ -26,26 +26,35 @@ app.get('/api/token/:token_id', function(req, res) {
   const month = parseInt(bdayParts[0])
   const data = {
     'name': person.name,
-    'attributes': {
-      'birthday': person.birthday,
-      'birth month': monthName(month),
-      'zodiac sign': zodiac(day, month),
+    'attributes': [
+      {
+        'trait_type': 'birthday',
+        'value': person.birthday
+      },
+      {
+        'trait_type': 'birth month',
+        'value': monthName(month)
+      },
+      {
+        'trait_type': 'zodiac sign',
+        'value': zodiac(day, month)
+      },
       // 'age': moment().diff(person.birthday, 'years')
-    },
+    ],
     'image': `${HOST}/images/${tokenId}.png`
   }
   res.send(data)
 })
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 })
 
 // returns the zodiac sign according to day and month ( https://coursesweb.net/javascript/zodiac-signs_cs )
 function zodiac(day, month) {
-  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
-  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
+  var zodiac = ['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
+  var last_day = ['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
+  return (day > last_day[month]) ? zodiac[month * 1 + 1] : zodiac[month];
 }
 
 function monthName(month) {
